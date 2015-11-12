3c3ede969a5570366b537f8f4766a71b5dcc06e2
zlodiak <prozaik81-2@yandex.ru>
2015-11-12 22:42:34 +0300

    pagination ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts, dependent: :destroy
-  belongs_to :user_status_id
+  belongs_to :user_status
 end
