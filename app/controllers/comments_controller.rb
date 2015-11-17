7bd12a29971e4f618e6140715626736c28b58458
zlodiak <prozaik81-2@yandex.ru>
2015-11-17 11:33:42 +0300

    delete actions in comments controller/ delete coreesponds views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,14 @@
 class CommentsController < InheritedResources::Base
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @comments = Comment.all
-    @comment = Comment.new
-    # binding.pry
-  end
-
-  def show
-  end
-
   def new
     @comment = Comment.new(:parent_id => params[:parent_id])
     @parent_id = params[:parent_id]
     @post = Post.find(params[:post_id])
   end
 
-  def edit
-  end
-
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.post_id = comment_params[:post_id]
-    p '------------------------------'
-    p comment_params[:post_id]
 
     respond_to do |format|
       if @comment.save
@@ -34,26 +18,6 @@ class CommentsController < InheritedResources::Base
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'comment was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
