465539020fc5167157e5314bf86a751be9b720e9
zlodiak <prozaik81-2@yandex.ru>
2015-11-23 11:52:35 +0300

    add flash to update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,20 +33,43 @@ class PostsController < InheritedResources::Base
 
     if @post.save    
       add_new_tags(@post)  
-      redirect_to @post, notice: 'posts was successfully created.'
+      flash[:success] = 'Пост успешно создан'
+      redirect_to @post
     else
+      flash.now[:error] = 'При создании поста произошла ошибка'
       render :new 
     end
   end
 
+=begin
   def update
     begin  
+      p 22222222222222222222222222222222222222222
+      p post_params 
+      p post_params[:title]
       @post.update(post_params)
+      p @post
       add_new_tags(@post) if params[:tagnames]
       destroy_tags(params['delete_tags'], @post) if params['delete_tags']   
-      redirect_to user_post_path(current_user.id, @post.id), notice: 'posts was successfully updated.'
+      flash[:success] = 'Пост успешно обновлён'
+      redirect_to user_post_path(current_user.id, @post.id)
     rescue  
+      p 33333333333333333333333333333333333333333333
       logger.debug 'update post is failed'
+      flash.now[:error] = 'При обновлении поста произошла ошибка'
+      render :edit
+    end
+  end
+=end
+
+  def update
+    if @post.update(post_params)
+      add_new_tags(@post) if params[:tagnames]  
+      destroy_tags(params['delete_tags'], @post) if params['delete_tags']    
+      flash[:success] = 'Пост успешно обновлён'
+      redirect_to user_post_path(current_user.id, @post.id)
+    else
+      flash.now[:error] = 'При обновлении поста произошла ошибка'
       render :edit
     end
   end
@@ -94,7 +117,30 @@ class PostsController < InheritedResources::Base
         error_403 unless current_user.id == @post.user_id   
       end
     end 
+    
+    def add_new_tags(post)
+      tagnames = params[:tagnames].split(/[, \.?!]+/)
+      tagnames.each do |tagname|
+        tag = Tag.find_by(title: tagname.downcase)
+        if tag
+          sql = "select * from 'posts_tags' where post_id = #{post.id} and tag_id = #{tag.id}"
+          records_array = ActiveRecord::Base.connection.execute(sql) 
+          tag.posts << post if records_array.count == 0  
+        else
+          tag = Tag.create(title: tagname.downcase)  
+          tag.posts << post     
+        end
+      end
+    end  
 
+    def destroy_tags(tags,post)
+      tags.each do |tag|   
+        sql = "delete from 'posts_tags' where post_id = #{post.id} and tag_id = #{tag}"
+        records_array = ActiveRecord::Base.connection.execute(sql)    
+      end
+    end 
+
+=begin
     def add_new_tags(post)
       tagnames = params[:tagnames].split(/[, \.?!]+/) 
       tagnames.each do |tagname|
@@ -130,6 +176,8 @@ class PostsController < InheritedResources::Base
           raise 'destroy_tags error'          
         end
       end
-    end        
+    end  
+=end
+
 end
 
