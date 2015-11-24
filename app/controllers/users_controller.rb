7f8c2088bc3af1ba05c1cf7551c84c808645258b
zlodiak <prozaik81-2@yandex.ru>
2015-11-24 11:23:46 +0300

    index with pager ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.where(user_status_id: 1).paginate(page: params[:page], :per_page => 10).order(created_at: :DESC)  
   end
 
   def new
